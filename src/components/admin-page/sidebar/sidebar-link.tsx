e80e3cbf823bdf7937ac99769b1bd345dc4aa48a
BoomTHDev <minecraft8231@gmail.com>
2025-03-04 21:41:27 +0700

    day17.3

--- src/components/admin-page/sidebar/sidebar-link.tsx
+++ src/components/admin-page/sidebar/sidebar-link.tsx
@@ -1,3 +1,7 @@
+import { Button } from '@/components/ui/button'
+import { cn } from '@/lib/utils'
+import Link from 'next/link'
+
 interface SidebarLinkProps {
   label: string
   href: string
@@ -6,6 +10,24 @@ interface SidebarLinkProps {
 }
 
 const SidebarLink = ({ label, href, icon, isActive }: SidebarLinkProps) => {
-  return <div>SidebarLink</div>
+  return (
+    <Button
+      variant={isActive ? 'secondary' : 'ghost'}
+      asChild
+    >
+      <Link
+        href={href}
+        className={cn(
+          'w-full justify-start gap-3',
+          isActive
+            ? 'font-semibold'
+            : 'text-muted-foreground hover:text-foreground'
+        )}
+      >
+        {icon}
+        <span>{label}</span>
+      </Link>
+    </Button>
+  )
 }
 export default SidebarLink
