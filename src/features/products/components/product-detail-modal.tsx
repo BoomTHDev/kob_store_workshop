09547c665642402942937226ca89f587cc78adfb
BoomTHDev <minecraft8231@gmail.com>
2025-03-17 21:25:30 +0700

    day26.14

--- src/features/products/components/product-detail-modal.tsx
+++ src/features/products/components/product-detail-modal.tsx
@@ -1,5 +1,7 @@
 import Modal from "@/components/shared/modal";
+import { Tabs, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { ProductType } from "@/types/product";
+import { TabsContent } from "@radix-ui/react-tabs";
 
 interface ProductDetailModalProps {
   open: boolean;
@@ -20,8 +22,23 @@ const ProductDetailModal = ({
       onOpenChange={onOpenChange}
       title={product.title}
       description={`SKU: ${product.sku}`}
+      className="sm:max-w-3xl"
     >
-      Test
+      <div>
+        <Tabs>
+          <TabsList>
+            <TabsTrigger value="overview">Overview</TabsTrigger>
+            <TabsTrigger value="details">Details</TabsTrigger>
+            <TabsTrigger value="images">
+              Images ({product.images.length})
+            </TabsTrigger>
+          </TabsList>
+
+          <TabsContent value="overview">Overview</TabsContent>
+          <TabsContent value="details">Details</TabsContent>
+          <TabsContent value="images">Images</TabsContent>
+        </Tabs>
+      </div>
     </Modal>
   );
 };
