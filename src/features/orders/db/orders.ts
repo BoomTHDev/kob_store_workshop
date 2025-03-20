8e8d89924aba8edfe5327e74926e1bf8fca6294c
BoomTHDev <minecraft8231@gmail.com>
2025-03-20 22:04:47 +0700

    day29.8

--- src/features/orders/db/orders.ts
+++ src/features/orders/db/orders.ts
@@ -1,6 +1,9 @@
 import { authCheck } from "@/features/auths/db/auths";
 import { redirect } from "next/navigation";
 import { canCreateOrder } from "../permissions/orders";
+import { checkoutSchema } from "../schemas/orders";
+import { db } from "@/lib/db";
+import { generateOrderNumber } from "@/lib/generateOrderNumber";
 
 interface CheckoutInput {
   address: string;
@@ -23,6 +26,38 @@ export const createOrder = async (input: CheckoutInput) => {
       input.address = user.address;
       input.phone = user.tel;
     }
+
+    const { success, data, error } = checkoutSchema.safeParse(input);
+
+    if (!success) {
+      return {
+        message: "กรุณากรอกข้อมูลให้ถูกต้อง",
+        error: error.flatten().fieldErrors,
+      };
+    }
+
+    const cart = await db.cart.findFirst({
+      where: { orderedById: user.id },
+      include: {
+        products: {
+          include: {
+            product: true,
+          },
+        },
+      },
+    });
+
+    if (!cart || cart.products.length === 0) {
+      return {
+        message: "ไม่มีสินค้าในตะกร้า",
+      };
+    }
+
+    const shippingFee = 50;
+
+    const orderNumber = generateOrderNumber();
+
+    console.log(orderNumber);
   } catch (error) {
     console.error("Error creating order:", error);
     return {
