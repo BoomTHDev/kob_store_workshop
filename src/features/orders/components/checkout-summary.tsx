a5397f36485ca483b340117103c5b6f14dcd9e57
BoomTHDev <minecraft8231@gmail.com>
2025-03-20 22:45:56 +0700

    day29.13

--- src/features/orders/components/checkout-summary.tsx
+++ src/features/orders/components/checkout-summary.tsx
@@ -1,4 +1,6 @@
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Separator } from "@/components/ui/separator";
+import { formatPrice } from "@/lib/formatPrice";
 import { CartType } from "@/types/cart";
 import Image from "next/image";
 
@@ -7,6 +9,10 @@ interface CheckoutSummaryProps {
 }
 
 const CheckoutSummary = ({ cart }: CheckoutSummaryProps) => {
+  const shippingFee = 50;
+
+  const totalAmount = cart.cartTotal + shippingFee;
+
   return (
     <Card>
       <CardHeader>
@@ -28,8 +34,40 @@ const CheckoutSummary = ({ cart }: CheckoutSummaryProps) => {
                   className="object-cover"
                 />
               </div>
+
+              <div className="flex-1 text-sm">
+                <div className="font-medium line-clamp-1">
+                  {item.product.title}
+                </div>
+                <div className="text-muted-foreground">
+                  {item.count} x {formatPrice(item.product.price)}
+                </div>
+              </div>
+
+              <div className="font-medium">{formatPrice(item.price)}</div>
             </div>
           ))}
+        </div>
+
+        <Separator />
+
+        <div className="flex flex-col gap-2">
+          <div className="flex justify-between">
+            <span>ยอดรวมสินค้า</span>
+            <span>{formatPrice(cart.cartTotal)}</span>
+          </div>
+
+          <div className="flex justify-between">
+            <span>ค่าจัดส่ง</span>
+            <span>{formatPrice(shippingFee)}</span>
+          </div>
+
+          <Separator />
+
+          <div className="flex justify-between font-bold text-lg">
+            <span>รวมทั้งสิ้น</span>
+            <span>{formatPrice(totalAmount)}</span>
+          </div>
         </div>
       </CardContent>
     </Card>
