29e27585ef26c5c2cd51f772b4e6036c8720d345
BoomTHDev <minecraft8231@gmail.com>
2025-03-21 20:47:02 +0700

    day30.2

---
+++ src/features/orders/components/order-detail.tsx
@@ -0,0 +1,11 @@
+import { OrderType } from "@/types/order";
+
+interface OrderDetailProps {
+  order: OrderType;
+}
+
+const OrderDetail = ({ order }: OrderDetailProps) => {
+  return <></>;
+};
+
+export default OrderDetail;
